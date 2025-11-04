6ca8be74e57c6f530092f091ce8d848c278d6d59
Valdotorium <116170877+Valdotorium@users.noreply.github.com>
2025-11-04 23:56:15 +0100

    added maps

---
+++ allplaces.js
@@ -0,0 +1,58 @@
+  "places": [
+    {
+      "code": "SIN",
+      "name": "Singapore",
+      "description": "Build the Ideal Metro for this bustling and well-known city state.",
+      "bbox": [103.622273,1.236553,104.038255,1.471117], // -79.454498,43.624458,-79.310818,43.680412
+      "population": 6000000,
+    },
+    {
+      "code": "BER",
+      "name": "Berlin",
+      "description": "Build a modern U-Bahn for Germanys reunified capital.",
+      "bbox": [13.162520,52.343779,13.632360,52.619711], // -79.454498,43.624458,-79.310818,43.680412
+      "population": 3700000,
+    },
+    {
+      "code": "FUK",
+      "name": "Fukuoka",
+      "description": "Build a Metro for Fukuoka, the largest city on Japan's southern island Kyushu!",
+      "bbox": [130.274506,33.519358,130.503845,33.706634], // -79.454498,43.624458,-79.310818,43.680412
+      "population": 1600000,
+    },
+    {
+      "code": "LON",
+      "name": "London",
+      "description": "Rebuild the worlds oldest underground railway to achieve good service on all lines!",
+      "bbox": [-0.372162,51.372487,0.127716,51.606930], // -79.454498,43.624458,-79.310818,43.680412
+      "population": 8900000,
+    },
+    {
+      "code": "STR",
+      "name": "Stuttgart",
+      "description": "Tunnel underneath the Swabian metropolis to create a modern Stadtbahn network!",
+      "bbox": [9.056854,48.694133,9.295120,48.862908], // -79.454498,43.624458,-79.310818,43.680412
+      "population": 1000000,
+    },
+    {
+      "code": "AA",
+      "name": "Aalen",
+      "description": "thats where i live :D",
+      "bbox": [9.986572,48.756641,10.231705,48.908962], // -79.454498,43.624458,-79.310818,43.680412
+      "population": 70000,
+    },
+    {
+      "code": "YVR",
+      "name": "Vancouver",
+      "description": "coastal",
+      "bbox": [-123.272095,49.104107,-122.728100,49.371643], // -123.224258,49.198579,-123.023438,49.316569
+      "population": 2642825,
+    },
+    {
+      "code": "HKG",
+      "name": "Hong Kong",
+      "description": "awesome",
+      "bbox": [113.779221,22.096456,114.408531,22.561985],
+      "population": 7500700,
+    },
+  ],
--- config.js
+++ config.js
@@ -1,14 +1,49 @@
 const config = {
-  "subwaybuilderLocation": "C:\\Users\\runke\\AppData\\Local\\Programs\\Subway\ Builder\\", // appimage location image on linux or install directory on windows (something like C:\\Users\\[username]\\AppData\\Local\\Programs\\Subway\ Builder)
+  "subwaybuilderLocation": "/Applications/Subway Builder.app", // appimage location image on linux, .app location on mac or install directory on windows (something like C:\Users\[username]\AppData\Local\Programs\Subway Builder)
   "tile-zoom-level": 16, // zoom level for map tiles to download
-  "protomaps-bucket": "https://build.protomaps.com/20251023.pmtiles",
+  "protomaps-bucket": "https://build.protomaps.com/20251103.pmtiles",
   "places": [
     {
-      "code": "YWG",
-      "name": "Winnipeg",
-      "description": "chicago if it was tiny",
-      "bbox": [-97.334061,49.766631,-96.958466,49.977059], // -79.454498,43.624458,-79.310818,43.680412
-      "population": 850000,
+      "code": "SIN",
+      "name": "Singapore",
+      "description": "Build the Ideal Metro for this bustling and well-known city state.",
+      "bbox": [103.622273,1.236553,104.038255,1.471117], // -79.454498,43.624458,-79.310818,43.680412
+      "population": 6000000,
+    },
+    {
+      "code": "BER",
+      "name": "Berlin",
+      "description": "Build a modern U-Bahn for Germanys reunified capital.",
+      "bbox": [13.162520,52.343779,13.632360,52.619711], // -79.454498,43.624458,-79.310818,43.680412
+      "population": 3700000,
+    },
+    {
+      "code": "FUK",
+      "name": "Fukuoka",
+      "description": "Build a Metro for Fukuoka, the largest city on Japan's southern island Kyushu!",
+      "bbox": [130.274506,33.519358,130.503845,33.706634], // -79.454498,43.624458,-79.310818,43.680412
+      "population": 1600000,
+    },
+    {
+      "code": "LON",
+      "name": "London",
+      "description": "Rebuild the worlds oldest underground railway to achieve good service on all lines!",
+      "bbox": [-0.372162,51.372487,0.127716,51.606930], // -79.454498,43.624458,-79.310818,43.680412
+      "population": 8900000,
+    },
+    {
+      "code": "STR",
+      "name": "Stuttgart",
+      "description": "Tunnel underneath the Swabian metropolis to create a modern Stadtbahn network!",
+      "bbox": [9.056854,48.694133,9.295120,48.862908], // -79.454498,43.624458,-79.310818,43.680412
+      "population": 1000000,
+    },
+    {
+      "code": "AA",
+      "name": "Aalen",
+      "description": "thats where i live :D",
+      "bbox": [9.986572,48.756641,10.231705,48.908962], // -79.454498,43.624458,-79.310818,43.680412
+      "population": 70000,
     },
     {
       "code": "YVR",
@@ -24,15 +59,8 @@ const config = {
       "bbox": [113.779221,22.096456,114.408531,22.561985],
       "population": 7500700,
     },
-    {
-      "name": "Singapore",
-      "code": "SIN",
-      "description": "tropical",
-      "bbox": [103.595409,1.147308,104.091339,1.479128],
-      "population": 6800000,
-    }
   ],
-  "platform": "windows" // either 'linux' or 'windows'
+  "platform": "macos" // either 'linux' or 'windows' or 'macos'
 };
 
 export default config;
